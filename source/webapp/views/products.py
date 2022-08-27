9040f0c955fdd383b997613505eaee39051201b9
begimaib <begimai.bolotbekova>
2022-08-27 15:52:22 +0600

    models for Product and Review are created, migrated, views are created accordingly, templates and partials are created

---
+++ source/webapp/views/products.py
@@ -0,0 +1,54 @@
+from django.urls import reverse_lazy, reverse
+from django.views.generic import UpdateView, DeleteView, DetailView, CreateView
+
+from webapp.forms import ProductForm
+from webapp.models import Product
+from webapp.views.base_view import SearchView
+
+
+class IndexView(SearchView):
+    model = Product
+    template_name = 'product/index.html'
+    ordering = ['category', 'name']
+    search_fields = ['name__icontains']
+    paginate_by = 3
+    context_object_name = 'products'
+
+    # def get_queryset(self):
+    #     return super().get_queryset().filter(amount__gt=0)
+
+
+class ProductView(DetailView):
+    model = Product
+    template_name = 'product/product_view.html'
+    # queryset = Product.objects.filter(amount__gt=0)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        id = self.object.id
+        context['reviews'] = Product.objects.filter(id=id)
+        return context
+
+
+class ProductCreate(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'product/product_create.html'
+
+    def get_success_url(self):
+        return reverse('webapp:product_view', kwargs={'pk': self.object.pk})
+
+
+class ProductUpdate(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'product/product_update.html'
+
+    def get_success_url(self):
+        return reverse('webapp:product_view', kwargs={'pk': self.object.pk})
+
+
+class ProductDelete(DeleteView):
+    model = Product
+    template_name = 'product/product_delete.html'
+    success_url = reverse_lazy('webapp:index')
---
+++ source/webapp/views/reviews.py
@@ -0,0 +1,45 @@
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.shortcuts import get_object_or_404
+from django.urls import reverse
+from django.views.generic import CreateView, UpdateView, DeleteView
+
+from webapp.forms import ReviewForm
+from webapp.models import Product, Review
+
+
+class CreateReview(CreateView):
+    form_class = ReviewForm
+    template_name = "reviews/create.html"
+
+    def form_valid(self, form):
+        product = get_object_or_404(Product, pk=self.kwargs.get("pk"))
+        user = self.request.user
+        form.instance.product = product
+        form.instance.author = user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse("webapp:product_view", kwargs={"pk": self.object.product.pk})
+
+
+class UpdateReview(UserPassesTestMixin, UpdateView):
+    form_class = ReviewForm
+    template_name = "reviews/update.html"
+    model = Review
+
+    def test_func(self):
+        return self.get_object().author == self.request.user or\
+               self.request.user.has_perm('webapp.change_review')
+
+    def get_success_url(self):
+        return reverse("webapp:product_view", kwargs={"pk": self.object.product.pk})
+
+
+class DeleteReview(DeleteView):
+    model = Review
+
+    def get(self, request, *args, **kwargs):
+        return super().delete(request, *args, **kwargs)
+
+    def get_success_url(self):
+        return reverse("webapp:product_view", kwargs={"pk": self.object.ptoduct.pk})
